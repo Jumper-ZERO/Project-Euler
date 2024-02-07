345e5322c7d2a47956dd72ca15322523f1eb3c3b
Jeremi Aron <chancanjeremiaron@gmail.com>
2024-02-07 12:34:01 -0500

    add problem 8

---
+++ Code/Problem 8 - Largest product in a series/LargestProductInASeries.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class LargestProductInASeries {
+
+    public static long resolve(int n) {
+		// coding here
+	}
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int t = in.nextInt();
+        for(int a0 = 0; a0 < t; a0++){
+            int n = in.nextInt();
+            System.out.println(resolve(n));
+        }
+    }
+}
---
+++ Code/Problem 8 - Largest product in a series/largest_product_in_a_series.py
@@ -0,0 +1,9 @@
+import sys
+
+def largest_product_in_a_series(n):
+	pass # coding here
+
+t = int(input().strip())
+for a0 in range(t):
+    n = int(input().strip())
+    print(largest_product_in_a_series(n))
